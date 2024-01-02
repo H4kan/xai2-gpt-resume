0947b70af1be2f456cdf1fd3ef0995f8b50b9851
Szymon Sieradzki <szsie@eg.dk>
2024-01-02 23:31:26 +0100

    add sample data

--- research.py
+++ research.py
@@ -2,23 +2,26 @@
 from pdf.processing import PdfProcessor
 from pdf.anonymizer import Anonymizer
 from chatbot.chatbot import ChatBot
+import os
 
 anonymizer = Anonymizer()
 chatbot = ChatBot()
 
 direc = "./research/pdfs/"
-file_paths = ["samplecv.pdf"]
 i = 0
 
-for file_path in file_paths:
-    i = i + 1
-    processor = PdfProcessor(direc + file_path)
-    fText = processor.scrap_text()
-    fText = anonymizer.anonymize_text(fText)
-    pure_response = chatbot.get_pure(fText)
+for file_path in os.listdir(direc):
+    if file_path.endswith('.pdf'):
+        i = i + 1
+        processor = PdfProcessor(direc + file_path)
+        fText = processor.scrap_text()
+        fText = anonymizer.anonymize_text(fText)
+        pure_response = chatbot.get_pure(fText)
 
-    with open(f"./research/data/scrap{i}.txt", 'w') as file:
-        file.write(fText)
-    
-    with open(f"./research/data/res{i}.txt", 'w') as file:
-        file.write(pure_response)
+        with open(f"./research/data/scrap{i}.txt", 'w', encoding='utf-8') as file:
+            file.write(fText)
+        
+        with open(f"./research/data/res{i}.txt", 'w', encoding='utf-8') as file:
+            file.write(pure_response)
+
+        print(f"Finished itertation {i}")
